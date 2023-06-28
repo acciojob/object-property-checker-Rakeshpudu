6d095db6cad61ac7a31231c2be83a174023a2d7d
Rakeshpudu <117848570+Rakeshpudu@users.noreply.github.com>
2023-06-28 19:29:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,16 +5,20 @@
 // }
 
 // // Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// 
+
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
+ 
 function hasKey(key) {
-	for(let i in sampleObject){
-        if(i === key) return true 
-    }
-    return false ;
+  //   write your code here
+       for(let k in sampleObject ){
+		   if(k===key){
+			   return true;
+		   }
+	   }
+	   return false;
 }
-
+ 
 // Do not change the code below
 const key = prompt("Enter Key.");
+alert(hasKey(key));
